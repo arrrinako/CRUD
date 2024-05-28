ec80f9573c5de4e93664e4f966715ecda98e2140
arrrinako <arina.kokhanova@yandex.ru>
2024-05-28 20:50:12 +0300

    8 неделя

--- frontend/app/api/config.js
+++ frontend/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api-code-2.practicum-team.ru'
+export const BASE_URL = 'http://arfront.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,

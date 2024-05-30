a0047bfbae7ec938a339dfd6d3b028cf76e9ec0f
arrrinako <arina.kokhanova@yandex.ru>
2024-05-30 12:55:17 +0300

    Update api-utils.js

--- frontend/app/api/api-utils.js
+++ frontend/app/api/api-utils.js
@@ -15,12 +15,14 @@ export const isResponseOk = (response) => {
   return !(response instanceof Error)
 }
 
+
 const normalizeDataObject = (obj) => {
-  return {
-    ...obj,
-    category: obj.categories,
-    users: obj.users_permissions_users,
-  }
+  let str = JSON.stringify(obj)
+  
+  str = str.replaceAll('_id', 'id');
+  const newObj = JSON.parse(str)
+  const result = { ...newObj, category: newObj.categories }
+  return result;
 }
 
 export const normalizeData = (data) => {
